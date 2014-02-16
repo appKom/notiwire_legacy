6813b8e9c79a9cd75f830b665b07b7852b8230a2
Michael Johansen <michael@informatikk.org>
2014-02-16 22:50:02 +0100

    get_affiliations: shell script for fetching the name of all affiliations in Notifier

---
+++ get_affiliations.sh
@@ -0,0 +1,19 @@
+# Online Notiwire
+# Shell script for getting all affiliations
+# appKom: appkom@online.ntnu.no
+# author: michael@informatikk.org
+
+echo ""
+
+# curl fetches options.html from the online-notifier repo
+curl -s https://raw.github.com/dotKom/online-notifier/master/options.html |
+# grep finds all lines matching values in dropdown menues at the options page
+grep "<option value=\".*\">.*</option>" |
+# grep again finds all values (affiliation keys) enclosed in those dropdown items
+grep -oEi "\".*\"" |
+# tr joins all lines by a comma
+tr "\\n" "," |
+# sed removes all entries after the first hit on "blue" (from the color palette dropdown menu in options.html)
+sed 's/"blue".*//'
+
+echo ""
